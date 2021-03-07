4a0991ebb03ad108c24ca4c6114e5c615b8e08c5
yamori-masato <yamorimasato@gmail.com>
2021-03-07 16:54:23 +0900

    useContextを追加

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import Layout from '../components/Layout/Layout'
 import { fetchPopularData } from '../apis'
+import { Store } from '../store'
 
 const Top = () => {
+  const { globalState, setGlobalState } = useContext(Store)
+
   useEffect(() => {
     fetchPopularData().then(res => {
       console.log('data', res)
+      setGlobalState({type: 'SET_POPULAR', payload: {popular: res.data.items}})
     })
-  }, [])
+  }, [setGlobalState])
 
   return (
     <Layout>
