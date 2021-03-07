785669677bef0018a4b5a95278eaab60ea457cb9
yamori-masato <yamorimasato@gmail.com>
2021-03-07 15:30:05 +0900

    Headerコンポーネントのスタイルを追加

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import Style from './Header.module.scss'
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className={Style.header}>
+      <div className={Style.item}>
+        <Link to='/'>VideoTube</Link>
+      </div>
+      <div className={Style.item}>
+        <form>
+          <input type="text" placeholder="検索" />
+          <button type="submit"><FontAwesomeIcon icon={faSearch} /></button>
+        </form>
+      </div>
     </div>
   )
 }
