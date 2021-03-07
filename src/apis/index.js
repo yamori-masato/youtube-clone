b4557d21c2cc882a89bd41d0e94807489c6d4c8d
yamori-masato <yamorimasato@gmail.com>
2021-03-07 16:30:56 +0900

    apiからデータを取得

---
+++ src/apis/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
+console.log(API_KEY)
+
+const youtube = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3'
+})
+
+export const fetchPopularData = async () => {
+  return await youtube.get('/videos', {
+    params: {
+      part: 'snippet',
+      maxResult: 40,
+      key: API_KEY,
+      regionCode: 'JP',
+      type: 'video',
+      chart: 'mostPopular'
+    }
+  })
+}
