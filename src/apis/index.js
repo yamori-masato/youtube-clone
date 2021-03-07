0ccc9b966c0adf820c66d8aa0f6791075c6a5bb8
yamori-masato <yamorimasato@gmail.com>
2021-03-07 16:47:49 +0900

    デバッグ行を削除

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
-console.log(API_KEY)
 
 const youtube = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3'
